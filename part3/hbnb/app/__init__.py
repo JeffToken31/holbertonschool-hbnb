8f19f344752e2383745dd5cfbe374005b1c83d38
jeff <jeffrey-31@hotmail.fr>
2025-06-30 10:16:34 +0200

    include config object in init and upgrade requirements including bcrypt

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,8 +6,9 @@ from app.api.v1.review import api as review_ns
 from app.api.v1.place import api as place_ns
 
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc="/api/v1/docs")
 
     # Register the users namespace
