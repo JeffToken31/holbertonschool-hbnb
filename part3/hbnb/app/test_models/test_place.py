80b11b757df84fe2a4b23b226a54c5308105916f
jeff <jeffrey-31@hotmail.fr>
2025-06-30 09:23:29 +0200

    copy part2 into part3

---
+++ part3/hbnb/app/test_models/test_place.py
@@ -0,0 +1,30 @@
+from app.models.place import Place
+from app.models.users import User
+from app.models.review import Review
+
+def test_place_creation():
+    owner = User(first_name="John", last_name="Doe", email="john.doe@example.com")
+    place = Place(title="Cozy Apartment", description="A nice place to stay", price=100.00, latitude=37.7749, longitude=-122.4194, owner=owner)
+
+    # Adding a review
+    review = Review(text="Great stay!", rating=5, place=place, user=owner)
+
+    owner.add_place(place)
+    place.add_review(review)
+    owner.add_review(review)
+
+    assert place.title == "Cozy Apartment"
+    assert place.price == 100.00
+    assert len(place.reviews) == 1
+    assert place.reviews[0].text == "Great stay!"
+    print("Place creation and relationship test passed!")
+    print("Owner: {} {}".format(owner.last_name, owner.first_name))
+    print("ID: {}\nCreated at: {}".format(owner.id, owner.created_at))
+    print("Your place:")
+    for plc in owner.places:
+        print("{}".format(plc))
+    print("Your review of this place {}:".format(place.title))
+    for rev in owner.reviews:
+        print("{}".format(rev))
+
+test_place_creation()
---
+++ part3/hbnb/app/tests/test_user.py
@@ -0,0 +1,85 @@
+import unittest
+from app import create_app
+
+
+class TestUserEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_user(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com"
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_user_invalid_data(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "",
+            "last_name": "",
+            "email": "invalid-email"
+        })
+        self.assertEqual(response.status_code, 400)
+
+    def test_list_all_users(self):
+        response = self.client.get('/api/v1/users/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_users_by_id(self):
+        user = self.client.post('/api/v1/users/', json={
+            "first_name": "Jan",
+            "last_name": "Goe",
+            "email": "jan.goe@example.com"
+        })
+        self.assertEqual(user.status_code, 201)
+        user_uniq = user.get_json()
+        user_id = user_uniq['id']
+        response = self.client.get(f'/api/v1/users/{user_id}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_user_by_invalid_id(self):
+        response = self.client.get('/api/v1/users/abcdef')
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_user(self):
+        user = self.client.post('/api/v1/users/', json={
+            "first_name": "Kan",
+            "last_name": "Poe",
+            "email": "kan.poe@example.com"
+        })
+        self.assertEqual(user.status_code, 201)
+        user_uniq = user.get_json()
+        user_id = user_uniq['id']
+        response = self.client.put(f'/api/v1/users/{user_id}', json={
+            "first_name": "Van",
+            "last_name": "Poe",
+            "email": "Van.poe@example.com"
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_user_not_found(self):
+        response = self.client.put('/api/v1/users/user_id', json={
+            "first_name": "Van",
+            "last_name": "Poe",
+            "email": "Van.poe@example.com"
+        })
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_user_invalide_data(self):
+        user = self.client.post('/api/v1/users/', json={
+            "first_name": "Pan",
+            "last_name": "Moe",
+            "email": "pan.moe@example.com"
+        })
+        self.assertEqual(user.status_code, 201)
+        user_uniq = user.get_json()
+        user_id = user_uniq['id']
+        response = self.client.put(f'/api/v1/users/{user_id}', json={
+            "first_name": "",
+            "last_name": "zoe",
+            "email": "example.com"
+        })
+        self.assertEqual(response.status_code, 400)
