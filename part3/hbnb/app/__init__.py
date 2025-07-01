f468fd5535ebd4ea756fa8b2626cf7d87dfc8c91
jeff <jeffrey-31@hotmail.fr>
2025-07-01 10:38:56 +0200

    update branch

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,22 +4,43 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenity_ns
 from app.api.v1.review import api as review_ns
 from app.api.v1.place import api as place_ns
+from app.api.v1.auth import api as auth_ns
+from app.api.v1.protected import api as protected_ns
 from app.extends import bcrypt
+from app.extends import jwt
+from config import DevelopmentConfig
 
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc="/api/v1/docs")
-    bcrypt.init_app(app)
+    app.config["JWT_SECRET_KEY"] = "my-very-strong-and-long-secret-key-1234567890"
 
-    # Register the users namespace
+    authorizations = {
+        'BearerAuth': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization',
+            'description': 'Bearer <access_token>'
+        }
+    }
+    api = Api(app,
+            version='1.0',
+            title='HBnB API',
+            description='HBnB Application API',
+            doc="/api/v1/docs",
+            authorizations=authorizations,
+            security='BearerAuth')
+
+
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
     api.add_namespace(users_ns, path='/api/v1/users')
-    # Register the amenity namespace
     api.add_namespace(amenity_ns, path='/api/v1/amenities')
-    # Register the review namespace
     api.add_namespace(review_ns, path='/api/v1/reviews')
-    # Register the review namespace
     api.add_namespace(place_ns, path='/api/v1/places')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(protected_ns, path='/api/v1/protected')
 
     return app
