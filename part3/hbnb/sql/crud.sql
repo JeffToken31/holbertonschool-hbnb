bdd8be3b64478caf935663da79d7c8cd76aed8d3
jeff <jeffrey-31@hotmail.fr>
2025-07-06 21:24:30 +0200

    update password and create crud file containig select operations

--- part3/hbnb/sql/crud.sql
+++ part3/hbnb/sql/crud.sql
@@ -1,6 +1,73 @@
--- Read datas from tables on database development
+-- Basics SELECT from tables on database development
 SELECT
     first_name,
     email
 FROM
-    users;
+    users
+ORDER BY
+    first_name;
+
+SELECT
+    title,
+    price
+FROM
+    places
+WHERE
+    price < 120;
+
+SELECT
+    name
+FROM
+    amenities;
+
+SELECT
+    place_id
+FROM
+    place_amenity;
+
+SELECT
+    text,
+    rating
+FROM
+    reviews;
+
+-- Advanced SELECT with JOIN from tables on database development
+SELECT
+    first_name,
+    last_name,
+    title,
+    price
+FROM
+    users
+    JOIN places ON users.id = places.owner_id;
+
+SELECT
+    first_name,
+    last_name,
+    rating,
+    text
+FROM
+    users
+    JOIN reviews ON users.id = reviews.user_id;
+
+SELECT
+    first_name,
+    title,
+    rating,
+    text
+FROM
+    users
+    JOIN places ON users.id = places.owner_id
+    JOIN reviews ON users.id = reviews.user_id;
+
+SELECT
+    title,
+    name
+FROM
+    places
+    JOIN place_amenity ON places.id = place_amenity.place_id
+    JOIN amenities ON amenities.id = place_amenity.amenity_id
+ORDER BY
+    places.title;
+
+-- UPDATE from tables on database development
